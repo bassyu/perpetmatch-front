86a6248fb7e75e09b27323331de9bf9cb3a77d47
bassyu <yrkdgus@gmail.com>
2020-11-13 22:15:11 +0900

    Fix OAuth2 redirection for profile loading

--- src/components/auth/OAuth2Redirection.js
+++ src/components/auth/OAuth2Redirection.js
@@ -2,13 +2,17 @@ import React from 'react';
 import queryString from 'query-string';
 import client from '../../lib/api/client';
 import { withRouter } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { getUser } from '../../modules/profile';
 
 const OAuth2Redirection = ({ location, history }) => {
+  const dispatch = useDispatch();
   const tokenType = 'Bearer';
   const accessToken = queryString.parse(location.search).token;
 
   if (accessToken) {
     client.defaults.headers['Authorization'] = `${tokenType} ${accessToken}`;
+    dispatch(getUser());
     history.push('/signup/complete');
     try {
       localStorage.setItem('auth', JSON.stringify({ tokenType, accessToken }));
